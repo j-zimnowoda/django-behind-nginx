bf7e83523512b844aa7bee0fe43c70c7e3bbfe6c
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2019-09-03 10:20:38 +0200

    [diagnostics] - print diagnostics info to console

--- mysite/diagnostics/urls.py
+++ mysite/diagnostics/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dummy_url', views.dummy_url, name='dummy_url'),
 ]
--- mysite/diagnostics/views.py
+++ mysite/diagnostics/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from pprint import pprint
 
 # Create your views here.
-def index():
+from django.urls.base import reverse
+
+
+def index(request):
+    info = get_diagnostic_info(request=request)
+    pprint(info)
+
+    return HttpResponse("Diagnostics were printed to your console")
+
+
+def dummy_url(request):
+    return HttpResponse("Dummy url")
+
+
+def get_diagnostic_info(request):
+    return {
+
+        'request.META': request.META,
+        'request.get_host()': request.get_host(),
+        'request.build_absolute_uri()': request.build_absolute_uri(reverse('dummy_url'))
+    }
